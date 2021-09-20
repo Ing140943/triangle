8b002bb61005b0f87cd5adff02596b11887e1d54
Ing140943 <skladeeing@gmail.com>
2021-09-20 11:44:43 +0700

    Format code

--- triangle_test.py
+++ triangle_test.py
@@ -21,16 +21,12 @@ class TriangleTest(unittest.TestCase):
         """any non-positive argument should raise ValueError"""
         with self.assertRaises(ValueError):
             b1 = is_triangle(-1, 2, 2)
-        b2 = is_triangle( 1, 0, 2)
-        self.assertFalse(b2)
-            # b2 = is_triangle( 1, 0, 2)
-
+        with self.assertRaises(ValueError):
+            b2 = is_triangle( 1, 0, 2)
         with self.assertRaises(ValueError):
             b1 = is_triangle( 1, -1, 2)
-
         with self.assertRaises(ValueError):
             b1 = is_triangle( 1, 2, -1)
-
         with self.assertRaises(ValueError):
             b1 = is_triangle( -1, -1, -1)
         with self.assertRaises(ValueError):
